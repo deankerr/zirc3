542a9ae0b61ba19aab3279dae3ab2c47b8bc0baa
Dean Kerr <dean.kerr@gmail.com>
2025-12-12 19:47:07 +1100

    initial commit

---
+++ apps/web/src/routes/__root.tsx
@@ -0,0 +1,20 @@
+import Header from "@/components/header";
+import { Outlet, createRootRouteWithContext } from "@tanstack/solid-router";
+import { TanStackRouterDevtools } from "@tanstack/solid-router-devtools";
+export interface RouterContext {}
+
+export const Route = createRootRouteWithContext<RouterContext>()({
+	component: RootComponent,
+});
+
+function RootComponent() {
+	return (
+		<>
+			<div class="grid grid-rows-[auto_1fr] h-svh">
+				<Header />
+				<Outlet />
+			</div>
+			<TanStackRouterDevtools />
+		</>
+	);
+}
